b3187235c89e8500d5f6f4abc2b8200d4e59dde1
Phil <philipsilberman@gmail.com>
2018-04-28 18:48:40 -0500

    pybank.py returns correct info now

--- PyBank/pybank.py
+++ PyBank/pybank.py
@@ -10,9 +10,41 @@ with open(csvpath, newline='') as csvfile:
 
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    totalRevenue = 0
+    # Initialize all the variables for tracking
+    counter = 0
+    revenue = []
+    months = []
+    revChange = []
+
 
     for row in csvreader:
-        totalRevenue = totalRevenue + int(row[1])
+        months.append(row[0])
+        revenue.append(int(row[1]))
+    
+    # create a list for change in revenue
+    for i in range(len(revenue)-1):
+        thismonth = revenue[i]
+        nextmonth = revenue[i+1]
+        revChange.append(nextmonth-thismonth)
 
+    totalRevenue = sum(revenue)
+
+    maxDecrease = min(revChange)
+    maxDecIndex = revChange.index(maxDecrease)
+    maxDecMonth = months[maxDecIndex + 1]   # have to add one since the revchange list is one month ahead
+
+    maxIncrease = max(revChange)
+    maxIncIndex = revChange.index(maxIncrease)
+    maxIncMonth = months[maxIncIndex]   # have to add one since the revchange list is one month ahead
+
+    avgChange = round(sum(revChange)/float(len(revChange)), 0)
+        
+
+
+print("Financial Analysis")
+print("-" * 20)
+print(f'Total Months: {len(months)}')
 print(f'Total Revenue: ${totalRevenue}')
+print(f'Average Revenue Change: ${avgChange}')
+print(f'Greatest Increase in Revenue: {maxIncMonth} (${maxIncrease})')
+print(f'Greatest Decrease in Revenue: {maxDecMonth} (${maxDecrease})')
